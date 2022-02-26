7a237480cbe2ef368c4c0474d1ef577e4290b994
stuart-rickard <stuart@bau-dev.com>
2022-02-26 15:40:42 -0800

    working draft

--- script.js
+++ script.js
@@ -1,20 +1,74 @@
 let tableEl = document.getElementById("table");
 
-for (x = 1; x <= 20; x++) {
+const rows = 40;
+const columns = 40;
+
+const percentRed = 0.03;
+const percentYellow = 0.06;
+const percentBlue = 0.02;
+
+let redColumns = [];
+let yellowColumns = [];
+let blueColumns = [];
+
+let redRows = [];
+let yellowRows = [];
+let blueRows = [];
+
+for (x = 1; x <= rows; x++) {
+  if (Math.random() < percentRed) {
+    redRows.push(x);
+  } else {
+    if (Math.random() < percentRed + percentYellow) {
+      yellowRows.push(x);
+    } else {
+      if (Math.random() < percentRed + percentYellow + percentBlue) {
+        blueRows.push(x);
+      }
+    }
+  }
+}
+
+for (y = 1; y <= rows; y++) {
+  if (Math.random() < percentRed) {
+    redColumns.push(y);
+  } else {
+    if (Math.random() < percentRed + percentYellow) {
+      yellowColumns.push(y);
+    } else {
+      if (Math.random() < percentRed + percentYellow + percentBlue) {
+        blueColumns.push(y);
+      }
+    }
+  }
+}
+
+for (x = 1; x <= rows; x++) {
   let rowElement = document.createElement("tr");
   tableEl.appendChild(rowElement);
 
-  for (y = 1; y <= 20; y++) {
+  for (y = 1; y <= columns; y++) {
     let colElement = document.createElement("td");
-    // colElement.innerText = "-";
     colElement.setAttribute("data-x", x);
     colElement.setAttribute("data-y", y);
-    // colElement.style.padding = "0px 0px 0px 0px";
-    // colElement.setAttribute("padding", "0px");
-    let color = Math.floor(Math.random() * 16777215).toString(16);
-    colElement.style.backgroundColor = "#" + color;
+    if (redRows.includes(x)) {
+      colElement.style.backgroundColor = "#e4071e";
+    }
+    if (yellowRows.includes(x)) {
+      colElement.style.backgroundColor = "#Fbe90e";
+    }
+    if (blueRows.includes(x)) {
+      colElement.style.backgroundColor = "#0051f4";
+    }
+    if (redColumns.includes(y)) {
+      colElement.style.backgroundColor = "#e4071e";
+    }
+    if (yellowColumns.includes(y)) {
+      colElement.style.backgroundColor = "#Fbe90e";
+    }
+    if (blueColumns.includes(y)) {
+      colElement.style.backgroundColor = "#0051F4";
+    }
     rowElement.appendChild(colElement);
-
-    // console.log(x + " " + y);
   }
 }
